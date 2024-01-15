6aca018d303c8735a742f8bfd926fc6345968893
Nickolsky <romb.intu@gmail.com>
2024-01-15 13:43:56 +0300

    patch warns

--- internal/__init__.py
+++ internal/__init__.py
@@ -1,5 +1,5 @@
 import os
 from .store import Database, Metadata
 from .store import logger
-from .bot import start_bot, notify
+from .bot import start_bot, notify, db
 from .image import Text, draw
